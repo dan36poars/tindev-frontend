385566dc5a2b3d8638aebf301d6d82c0a15e753d
Daniel Corrêa <eldestemido@gmail.com>
2019-10-04 00:59:19 -0300

    local server fix and prod server ready

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,6 @@ const axios = require('axios');
 
 const environment = process.env.NODE_ENV === "development" ? 'http://localhost:3333' :  process.env.REACT_APP_API_URL ;
 
-console.log(environment);
-
 const api = axios.create({
 	baseURL: environment
 });
